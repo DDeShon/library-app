4622d4460d6e984d217915f99cc9966a5dc1c082
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-04-27 09:47:27 -0500

    added functionality for image storage

--- public/scripts/fileUpload.js
+++ public/scripts/fileUpload.js
@@ -4,4 +4,10 @@ FilePond.registerPlugin(
   FilePondPluginFileEncode
 );
 
+FilePond.setOptions({
+  stylePanelAspectRatio: 150 / 100,
+  imageResizeTargetWidth: 100,
+  imageResizeTargetWidth: 150,
+});
+
 FilePond.parse(document.body);
