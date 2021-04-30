28f4eafe9e5936e1e16a1f9ae5b3a0d676ed6074
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-04-29 23:08:59 -0500

    added update functionality

--- routes/authors.js
+++ routes/authors.js
@@ -31,8 +31,7 @@ router.post("/", async (req, res) => {
   });
   try {
     const newAuthor = await author.save();
-    // res.redirect(`authors/${newAuthor.id}`)
-    res.redirect("authors");
+    res.redirect(`authors/${newAuthor.id}`);
   } catch {
     res.render("authors/new", {
       author: author,
@@ -54,8 +53,23 @@ router.get("/:id/edit", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  res.send("Update Author " + req.params.id);
+router.put("/:id", async (req, res) => {
+  let author;
+  try {
+    author = await Author.findById(req.params.id);
+    author.name = req.body.name;
+    await author.save();
+    res.redirect(`/authors/${author.id}`);
+  } catch {
+    if (author == null) {
+      res.redirect("/");
+    } else {
+      res.render("authors/edit", {
+        author: author,
+        errorMessage: "Error updating author",
+      });
+    }
+  }
 });
 
 router.delete("/:id", (req, res) => {
