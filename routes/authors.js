70b79a7dc7fc412cdedd0baec016f0253b3583a3
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-04-29 22:36:48 -0500

    started author show/edit/delete functionality

--- routes/authors.js
+++ routes/authors.js
@@ -41,4 +41,20 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.get("/:id", (req, res) => {
+  res.send("Show Author " + req.params.id);
+});
+
+router.get("/:id/edit", (req, res) => {
+  res.send("Edit Author " + req.params.id);
+});
+
+router.put("/:id", (req, res) => {
+  res.send("Update Author " + req.params.id);
+});
+
+router.delete("/:id", (req, res) => {
+  res.send("Delete Author " + req.params.id);
+});
+
 module.exports = router;
