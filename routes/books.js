62c81b70760f08ead1916bd51b283fffe31355ba
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-05-02 09:52:32 -0500

    started book show functionality

--- routes/books.js
+++ routes/books.js
@@ -53,6 +53,15 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id).populate("author").exec();
+    res.render("books/show", { book: book });
+  } catch {
+    res.redirect("/");
+  }
+});
+
 async function renderNewPage(res, book, hasError = false) {
   try {
     const authors = await Author.find({});
