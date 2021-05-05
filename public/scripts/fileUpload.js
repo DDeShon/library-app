b4f8d0a24859c5cf464d0d1f1a1098bfd9a746f2
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-05-05 11:46:54 -0500

    added styling for new book page

--- public/scripts/fileUpload.js
+++ public/scripts/fileUpload.js
@@ -7,7 +7,7 @@ FilePond.registerPlugin(
 FilePond.setOptions({
   stylePanelAspectRatio: 150 / 100,
   imageResizeTargetWidth: 100,
-  imageResizeTargetWidth: 150,
+  imageResizeTargetHeight: 150,
 });
 
 FilePond.parse(document.body);
